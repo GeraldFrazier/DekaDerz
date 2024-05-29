6372d1de73363302c4fdb0ea1f669cfc5cc09fd4
Gerald Frazier Jr <geraldfraziercodes@gmail.com>
2024-05-29 16:58:14 -0700

    bug fixes

--- app/soundcloud/page.tsx
+++ app/soundcloud/page.tsx
@@ -19,24 +19,27 @@ const SoundCloudPlayer = () => {
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {tracks.map((track, index) => (
-          <div key={index} className="text-gray-400 text-xs overflow-hidden whitespace-nowrap overflow-ellipsis font-light font-sans">
+          <div
+            key={index}
+            className="text-gray-400 text-xs overflow-hidden whitespace-nowrap overflow-ellipsis font-light font-sans"
+          >
             <iframe
               width="300"
               height="166"
               allow="autoplay"
               className="w-full h-full"
-              src={`https://w.soundcloud.com/player/?url=${encodeURIComponent(track)}&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
+              src={`https://w.soundcloud.com/player/?url=${encodeURIComponent(
+                track
+              )}&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
             ></iframe>
           </div>
         ))}
       </div>
       <div className="mt-4">
-        <Link href="/youtube">
-          <a>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Videos
-            </button>
-          </a>
+        <Link href="/youtube" passHref>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Videos            
+</button>
         </Link>
       </div>
     </div>
